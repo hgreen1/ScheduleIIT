8aeb4243a2e3225dd36bc2d01e79811557de21f4
Henry R Green <hgreen1@hawk.iit.edu>
2017-10-30 16:18:58 -0500

    Added file

--- app/src/main/java/com/example/henry/scheduleiit/AddCRNActivity.java
+++ app/src/main/java/com/example/henry/scheduleiit/AddCRNActivity.java
@@ -3,13 +3,14 @@ package com.example.henry.scheduleiit;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+/**
+ * Created by Henry on 10/30/2017.
+ */
 
+public class AddCRNActivity extends AppCompatActivity{
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
-
-
 }
---
+++ app/src/main/java/com/example/henry/scheduleiit/CRNDatabase.java
@@ -0,0 +1,20 @@
+package com.example.henry.scheduleiit;
+
+/**
+ * Created by Henry on 10/30/2017.
+ */
+
+public class CRNDatabase {
+    /*
+    in this class, the mongoDB database will be created and populated each row has the
+    CRN (65820),
+    ClassName (Internet Application Development),
+    Department (ITM/CS/CE/BIOL),
+    ClassNumber (101),
+    Section (1),
+    TimeOfClass (in 24h time, i.e. 1350 - 1505), and
+    DaysOfWeek (the abbreviations are Su,Mo,Tu,We,Th,Fr,Sa -- Multiple days would be MoWeFr)
+    */
+
+
+}
